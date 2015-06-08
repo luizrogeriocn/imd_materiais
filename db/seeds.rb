4dab2fa6977dfe88a1ab02774e7495f298b4317e
Luiz Rogério <luizrogeriocn@gmail.com>
2015-06-08 20:58:55 -0300

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "coordinator@example.com", password: "rootadmin", nickname: "coordinator")
+User.create(email: "teacher1@example.com", password: "rootadmin", nickname: "teacher1")
+User.create(email: "teacher2@example.com", password: "rootadmin", nickname: "teacher2")
+
+Task.create(tilte: "Task1", description: "Task para prof 1", created_at: datetime, updated_at: datetime, status: 0)
+Task.create(tilte: "Task2", description: "Task para prof 2", created_at: datetime, updated_at: datetime, status: 0)
+Task.create(tilte: "Task3", description: "Task para prof 1 e 2", created_at: datetime, updated_at: datetime, status: 0)
+
+Chore.create(user_id: 1, task_id: 1, created_at: datetime, updated_at: datetime)
+Chore.create(user_id: 2, task_id: 2, created_at: datetime, updated_at: datetime)
+Chore.create(user_id: 1, task_id: 3, created_at: datetime, updated_at: datetime)
+Chore.create(user_id: 2, task_id: 3, created_at: datetime, updated_at: datetime)
+
+Role.create(title: "Coordinator", created_at: datetime, updated_at: datetime)
+Role.create(title: "Teacher", created_at: datetime, updated_at: datetime)
+
+Assignment.create(user_id: 1, role_id: 1, created_at: datetime, updated_at: datetime)
+Assignment.create(user_id: 2, role_id: 2, created_at: datetime, updated_at: datetime)
+Assignment.create(user_id: 3, role_id: 2, created_at: datetime, updated_at: datetime)
